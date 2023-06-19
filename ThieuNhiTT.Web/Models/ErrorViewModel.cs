83ec2fe0d1bdf49a892a41ebc0d6f3055e06f3ef
Tam Vu <tam.vu@hitachivantara.com>
2023-06-19 13:51:47 +0700

    commit first

---
+++ ThieuNhiTT.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThieuNhiTT.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
