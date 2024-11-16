005d967df4a6d5158264f0c1ea983d9809f480e5
Tam Vu <tamsps@gmail.com>
2024-11-17 00:06:00 +0700

    add lesson

---
+++ ThieuNhiTT.Web/Controllers/LessonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThieuNhiTT.Web.Services;
+
+namespace ThieuNhiTT.Web.Controllers
+{
+	public class LessonController : Controller
+	{
+
+		private readonly BookService _bookService;
+		private readonly LessonService _lessonService;
+
+		public LessonController(BookService bookService, LessonService lessonService)
+		{
+			_bookService = bookService;
+			_lessonService = lessonService;
+		}
+		public IActionResult Index(string bookId, string lessonId)
+		{
+			var lesson = _lessonService.GetLessonById(bookId, lessonId);
+
+			return View(lesson);
+		}
+	} 
+}
